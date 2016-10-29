56b4e5245aaffe9fffba3aee8a2a2ac14a6ac214
cbb103035 <cbb103035@stmail.nptu.edu.tw>
2016-10-29 22:56:28 +0800

    忘記改

--- src/ezerd/page.java
+++ src/ezerd/page.java
@@ -112,8 +112,8 @@ public class page extends Panel{
     }
     @Override
     public void paint(Graphics g) {
-        //bufferImage = createImage(parent.MainWin.ScreenSize.width, parent.MainWin.ScreenSize.height);
-        bufferImage = createImage(500, 500);
+        bufferImage = createImage(parent.MainWin.ScreenSize.width, parent.MainWin.ScreenSize.height);
+        //bufferImage = createImage(500, 500);
         bufferGraphics = bufferImage.getGraphics();
         parent.MainWin.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
         Graphics2D g2 = (Graphics2D)bufferGraphics;  
